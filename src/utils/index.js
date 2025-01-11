bd9f106a648af3320464fc29467234602fd79296
Marwan Wisnu <122840645+Mwannn@users.noreply.github.com>
2025-01-11 14:34:02 +0700

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,41 +6,6 @@ const getInitialData = () => ([
     createdAt: 'Jumat, 30 Juni 2006',
     archived: false,
   },
-/*   {
-    id: 2,
-    title: "Functional Component",
-    body: "Functional component merupakan React component yang dibuat menggunakan fungsi JavaScript. Agar fungsi JavaScript dapat disebut component ia harus mengembalikan React element dan dipanggil layaknya React component.",
-    createdAt: 'Jumat, 6 Juni 2023',
-    archived: false,
-  },
-  {
-    id: 3,
-    title: "Modularization",
-    body: "Dalam konteks pemrograman JavaScript, modularization merupakan teknik dalam memecah atau menggunakan kode dalam berkas JavaScript secara terpisah berdasarkan tanggung jawabnya masing-masing.",
-    createdAt: 'Sabtu, 7 Juni 2023',
-    archived: false,
-  },
-  {
-    id: 4,
-    title: "Lifecycle",
-    body: "Dalam konteks React component, lifecycle merupakan kumpulan method yang menjadi siklus hidup mulai dari component dibuat (constructor), dicetak (render), pasca-cetak (componentDidMount), dan sebagainya. ",
-    createdAt: 'Minggu, 8 Juni 2023',
-    archived: false,
-  },
-  {
-    id: 5,
-    title: "ESM",
-    body: "ESM (ECMAScript Module) merupakan format modularisasi standar JavaScript.",
-    createdAt: 'Senin, 9 Juni 2023',
-    archived: false,
-  },
-  {
-    id: 6,
-    title: "Module Bundler",
-    body: "Dalam konteks pemrograman JavaScript, module bundler merupakan tools yang digunakan untuk menggabungkan seluruh modul JavaScript yang digunakan oleh aplikasi menjadi satu berkas.",
-    createdAt: 'Selasa, 10 Juni 2023',
-    archived: false,
-  }, */
 ]);
 
 const showFormattedDate = (date) => {
